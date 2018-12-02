b9058e8fed4d26f60303a700c1fe3bb701a32b34
KhallilB <khallil.bailey@gmail.com>
2018-12-01 17:56:21 -0800

    load words function and user guesses

--- spaceMan.py
+++ spaceMan.py
@@ -6,16 +6,36 @@ def loadWords():
     Loads words from txt files,
     splits them and chooses a random one.
     '''
-    pass
+    file = open('wordsToGuess.txt', 'r')
+    line = file.readline()
+    wordList = line.split()
+    file.close()
+    secretWord = random.choice(wordList)
+    return secretWord
 
 
-def showHangman():
+def userGuesses(correctGuess, wrongGuess, secretWord):
     '''
     Uses secret word to Guess.
     Displays the number of letters correct,
      and incorrect letter guesses.
     '''
-    pass
+    print([len(wrongGuess)])
+
+    print('Wrong Guesses', end='')
+    for letter in wrongGuess:
+        print(letter, end='')
+
+    blanks = ' ' * len(secretWord)
+
+    for i in range(len(secretWord)):
+        if secretWord[i] in correctGuess:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:
+        print(letter, end='')
+
+    print()
 
 
 def guessedWord():
